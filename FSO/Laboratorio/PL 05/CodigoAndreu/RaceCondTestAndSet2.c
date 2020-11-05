d515224e049b5bcd68fd21c93efc6f00baf06c96
Andrac <56803936+andrac275@users.noreply.github.com>
2020-11-05 20:14:57 +0100

    Add files via upload

---
+++ FSO/Laboratorio/PL 05/CodigoAndreu/RaceCondTestAndSet2.c
@@ -0,0 +1,81 @@
+#include <unistd.h> 
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <semaphore.h>
+
+#define REPETITIONS 20000000   // CONSTANT
+
+// GLOBAL SHARED VARIABLES 
+long int V = 100;      // Initial value
+int spinlock = 0;   //al estar a 0 es que es FALSE i que la seccio critica NO esta ocupada.
+
+// AUXILIARY FUNTIONS
+int test_and_set(int *spinlock) {
+    int ret;
+    __asm__ __volatile__(
+    "xchg %0, %1"
+    : "=r"(ret), "=m"(*spinlock)
+    : "0"(1), "m"(*spinlock)
+    : "memory");
+    return ret;
+}
+
+// THREAD FUNCTIONS
+void *inc (void *parameter) {
+    long int cont, aux;
+    
+    while(test_and_set(&spinlock)){/*Bucle buit*/;} /*ProtocolEntrada*/
+    
+    for (cont = 0; cont < REPETITIONS; cont = cont + 1) {  
+        V = V + 1;/*Zona critica*/          
+    }
+    
+    spinlock = 0; /*ProtocolEixida*/
+    
+    printf("-------> inc end (V = %ld)\n", V);
+    pthread_exit(0);
+}
+
+void *dec (void *parameter) {
+    long int cont,aux;
+    
+    while(test_and_set(&spinlock)){/*Bucle buit*/;} /*ProtocolEntrada*/
+    
+    for (cont = 0; cont < REPETITIONS; cont = cont + 1) {
+        V = V - 1;/*Zona critica*/ 
+    }
+    
+    spinlock = 0; /*ProtocolEixida*/
+    
+    printf("-------> dec end (V = %ld)\n", V);
+    pthread_exit(0);
+}
+
+void *inspec (void *parameter) {
+    for (;;) {
+        usleep(200000);
+        fprintf(stderr, "Inspec: actual value of V = %ld\n", V);
+    } 
+}
+
+// MAIN FUNCTION  
+int main (void) {
+    // Declaring the requiered variables
+    pthread_t incThread, decThread, inspecThread;
+    pthread_attr_t attr;   
+  
+    // Default thread attributes
+     pthread_attr_init(&attr);
+ 
+    // EXERCISE: Create threads inc, dec and inspec with attr attributes 
+     pthread_create (&incThread, &attr, inc, 0);
+     pthread_create (&decThread, &attr, dec, 0);
+     pthread_create (&inspecThread, &attr, inspec, 0);
+    // EXERCISE: The main thread has to wait inc and dec threads to end
+     pthread_join (incThread, NULL);
+     pthread_join (decThread, NULL);
+    // Main program end
+    fprintf(stderr, "-------> FINAL VALUE: V = %ld\n\n", V);
+    exit(0);
+}   
